e0ddbf1069067caba1c5573226344605d6f969aa
snaztoz <hafidhmn@gmail.com>
2023-11-20 18:02:19 +0700

    feat: :sparkles: add simple command-line interface binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use colored::Colorize;
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!(
+            "{}",
+            "ERR: No input file supplied. Usage: `kaba <filename>`\n".red()
+        );
+        return;
+    }
+
+    let program = fs::read_to_string(&args[1]);
+    if let Err(e) = program {
+        eprintln!("{}\n", format!("ERR: {}", e.to_string()).red());
+        return;
+    }
+
+    kaba::run(&program.unwrap());
+    println!();
 }
