085a1f8b4bbc3745b83d9613c3304156e34040da
null8626 <vierofernando9@gmail.com>
2024-11-29 12:53:25 +0700

    refactor: use Self in match self statements

--- compiler/src/ast.rs
+++ compiler/src/ast.rs
@@ -439,10 +439,10 @@ pub enum Literal {
 impl Display for Literal {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Literal::Void => write!(f, "void"),
-            Literal::Integer(n) => write!(f, "{n}"),
-            Literal::Float(n) => write!(f, "{n}"),
-            Literal::Boolean(b) => write!(f, "{b}"),
+            Self::Void => write!(f, "void"),
+            Self::Integer(n) => write!(f, "{n}"),
+            Self::Float(n) => write!(f, "{n}"),
+            Self::Boolean(b) => write!(f, "{b}"),
         }
     }
 }
